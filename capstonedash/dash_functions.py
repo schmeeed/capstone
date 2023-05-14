65c28630576ea450dee5e68432604a03a23768db
Jason Phillip <jason.phillip@gmail.com>
2023-05-14 17:23:13 -0400

    Updated plotly dash" "

--- capstonedash/dash_functions.py
+++ capstonedash/dash_functions.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 from geopy.distance import geodesic
 from geopy.geocoders import Nominatim
-
 import boto3
 from botocore.exceptions import ClientError
 import json
